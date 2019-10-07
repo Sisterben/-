83f3536a315bb1a87abe6092a3d71bb85c076165
Sisterben <1031208128@qq.com>
2019-10-07 22:32:52 +0800

    Day6

---
+++ Day6.py
@@ -0,0 +1,22 @@
+name = 'Ann'
+age = '22'
+f'{name} is {age} years old.'#f-string
+pass
+sorted('abcd')
+sorted('abcd',reserve=True)
+pass
+divmod(11,3)
+a, b = divmod(11, 3)
+a
+b
+pass
+divmod(3, 11)
+a, b = divmod(3, 11)
+a
+b#divmod(a, b)a为商，b为余数
+pass
+pow(2, 3)
+pow(2, 3, 4)
+pass
+print("a \n b \n c")
+pass
