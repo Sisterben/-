43460ec144ac12a1de621ee0e09d0e74fab472cb
Sisterben <1031208128@qq.com>
2019-10-15 21:27:13 +0800

    Day9

---
+++ Day9.py
@@ -0,0 +1,29 @@
+#拼接字符串 str.join()
+s = ''
+t = ['P','y','t','h','o','n']
+s.join(t)#Q:s function
+pass
+#设定整行的宽度 str.center(width[, fillchar])
+s = 'Sparse is better than dense!'
+s.title().center(60)
+s.title().center(60, '=')
+s.title().center(10)
+s.title().rjust(60)
+s.title().rjust(60, '=')
+#str.rjust()or str.ljust()
+pass
+for o om range(1, 11):
+    filename = str(i).zfill(3) + '.mp3'
+    print(filename)
+pass
+#格式化字符串f.format() or f=string
+name = 'John'
+age = 25
+'{} is {} years old.'.format(name, age)
+f'{name} is {age} years old.'
+f'{name} is a grown up?{age >= 18}'
+pass
+for c in 'python'
+print(c)
+for i in range(10)
+print(i)
